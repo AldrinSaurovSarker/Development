6137e5377184fc42b2546b890803d09feb4b32fd
AldrinSaurovSarker <53370488+AldrinSaurovSarker@users.noreply.github.com>
2021-07-09 02:48:38 +0600

    Add files via upload

---
+++ Minesweeper/assets/js/description.js
@@ -0,0 +1,245 @@
+import { countRevealedTiles, setTimer, setGameOver, resetFlags, gameover, drawBoard,
+    setBtnProperties, flagAllSquares, unflagAllSquares, setDigitColors } from "./game.js";
+import { squares, run, game_over, game_started } from "./game.js";
+import { resultBox, content } from "./game.js";
+export var NUMBER_OF_BOMBS = 10;
+export var ROW = 9, COL = 9; //30 Col max
+export var timeElapsed = 0;
+
+const timer = document.querySelector('.timer');
+const gridForm = document.querySelector('.change-grid-form');
+const pauseScreen = document.querySelector('.pause-screen');
+const cancelBtn = document.querySelectorAll('.cancel');
+const restartLevel = document.querySelectorAll('.restart');
+const gridBtn = document.querySelectorAll('.change-grid-btn');
+const shuffleBoardBtn = document.querySelectorAll('.shuffle');
+
+var revealedTiles;
+
+
+// Set theme
+function setTheme(themeList) {
+    var theme = themeList.options[themeList.selectedIndex].value;
+    document.body.className = theme;
+    setDigitColors();
+}
+
+
+// Set the number of rows, colunms and bombs for different difficulty level
+function setBoardSize(difficulty) {
+    // Properties for built-in difficulties
+    const properties =  {
+        'beginner': [9, 9, 10],
+        'intermediate': [16, 16, 40],
+        'expert': [16, 30, 99]
+    }
+
+    // Setiings for custom difficulty
+    if (difficulty == 'custom') {
+        var x = document.getElementById("customForm");
+        
+        ROW = x.elements[0].value;
+        COL = x.elements[1].value;
+        NUMBER_OF_BOMBS = x.elements[2].value;
+
+        // Adjusting invalid inputs
+        if (ROW < 1) {
+            ROW = 1;
+            x.elements[0].value = ROW;
+        }
+            
+        if (COL < 1) {
+            COL = 1;
+            x.elements[1].value = COL;
+        }
+
+        if (NUMBER_OF_BOMBS > ROW * COL) {
+            NUMBER_OF_BOMBS = Math.floor((ROW * COL)/2);
+            x.elements[2].value = NUMBER_OF_BOMBS;
+        }
+    }
+
+    else {
+        ROW = properties[difficulty][0];
+        COL = properties[difficulty][1];
+        NUMBER_OF_BOMBS = properties[difficulty][2];
+    }
+    
+    content.classList.remove('blurred');
+    gridForm.classList.remove('fadeAnimation');
+    gridForm.style.visibility = 'hidden';
+
+    shuffleBoard();
+}
+
+
+// Change the position of bombs and reset all properties except ROW, COL & NUMBER_OF_BOMBS
+function shuffleBoard() {
+    drawBoard();
+    restartCurrentLevel();
+    setBtnProperties();
+    setTimer(false);
+}
+
+
+// Restart any level (all bombs and numbers are at the same position like a level currently is being played)
+function restartCurrentLevel() {
+    timeElapsed = 0;
+    setTimer(true);
+    setGameOver(false);
+    resetFlags();
+    unflagAllSquares();
+
+    resultBox.style.display = 'none';
+    content.classList.remove('blurred');
+
+    // Make all tiles clickable and numbers are hidden
+    for (let i = 0; i < ROW; i++) {
+        for (let j = 0; j < COL; j++) {
+            squares[i][j].status = 'clickable';
+            squares[i][j].draw.innerHTML = '';
+            squares[i][j].draw.classList.remove('revealed', 'bombed');
+            squares[i][j].draw.style.color = '#000000';
+        }
+    }
+}
+
+
+// Pause Game
+function pauseGame() {
+    setTimer(false);
+    content.classList.add('blurred');
+    pauseScreen.style.visibility = 'visible';
+}
+
+
+// Resume Game
+function resumeGame() {
+    if (game_started)
+        setTimer(true);
+    content.classList.remove('blurred');
+    pauseScreen.style.visibility = 'hidden';
+}
+
+
+// Show the popup of 'Change Grid Size'
+function adjustGrid() {
+    setTimer(false);
+
+    // Hide the game contents
+    if (!content.classList.contains('blurred'))
+        content.classList.add('blurred');
+
+    gridForm.style.visibility = 'visible';
+    gridForm.classList.add('fadeAnimation');
+}
+
+
+// Hide the popup of 'Change Grid Size'
+function cancelGridChange() {
+    // Show the game content is game is not over
+    if (!game_over) {
+        // Timer will be started if game was started before the popup showing
+        if (game_started)
+            setTimer(true);
+
+        content.classList.remove('blurred');
+    }
+
+    gridForm.classList.remove('fadeAnimation');
+    gridForm.style.visibility = 'hidden';
+}
+
+
+// Check if all numbers are revealed
+function checkForGameWin() {
+    if (ROW * COL - revealedTiles == NUMBER_OF_BOMBS) {
+        flagAllSquares();
+        setTimer(false);
+        gameover('Won');
+    }
+}
+
+
+// Change the 'Game info' board per 10 miliseconds
+setInterval(() => {
+    if (!game_over) {
+        document.getElementById('board-size').innerHTML = COL + 'x' + ROW; // Board size (Row, Col)
+        document.getElementById('total-cell').innerHTML = ROW * COL; // Number of tiles
+        document.getElementById('total-bomb').innerHTML = NUMBER_OF_BOMBS; // Number of bombs
+
+        try {
+            revealedTiles = countRevealedTiles();
+            document.getElementById('total-cell-covered').innerHTML = ROW * COL - revealedTiles; // Number of hidden tiles
+            document.getElementById('total-cell-revealed').innerHTML = revealedTiles; // Number of revealed tiles
+            checkForGameWin();
+        } catch {
+            document.getElementById('total-cell-covered').innerHTML = ROW * COL;
+            document.getElementById('total-cell-revealed').innerHTML = 0;
+        }
+    }
+}, 10);
+
+
+// Update time per second
+setInterval(() => {
+    if (run)
+        timeElapsed++;
+    timer.innerHTML = timeElapsed;
+}, 1000);
+
+
+// Reload the page if 'Change Board' button is clicked
+shuffleBoardBtn.forEach(btn => btn.addEventListener('click', function () {
+    shuffleBoard();
+}));
+
+// Restart the current level
+restartLevel.forEach(btn => btn.addEventListener('click', function () {
+    restartCurrentLevel();
+}));
+
+// Show popup for changing number of row, columns and bombs
+gridBtn.forEach(btn => btn.addEventListener('click', function () {
+    adjustGrid();
+}));
+
+// Cancel popup for changing number of row, columns and bombs
+cancelBtn.forEach(btn => btn.addEventListener('click', function () {
+    cancelGridChange();
+}));
+
+// Set board size for CUSTOM board
+document.querySelector('#custom .setBoard').addEventListener('click', function () {
+    setBoardSize('custom');
+});
+
+// Set board size for BEGINNER board
+document.querySelector('#beginner .setBoard').addEventListener('click', function () {
+    setBoardSize('beginner');
+});
+
+// Set board size for INTERMEDIATE board
+document.querySelector('#intermediate .setBoard').addEventListener('click', function () {
+    setBoardSize('intermediate');
+});
+
+// Set board size for EXPERT board
+document.querySelector('#expert .setBoard').addEventListener('click', function () {
+    setBoardSize('expert');
+});
+
+// Pause the game by clicking pause button
+document.querySelector('.pauseButton').addEventListener('click', function() {
+    pauseGame();
+});
+
+// Resume the game by clicking resume button
+document.querySelector('.resumeButton').addEventListener('click', function() {
+    resumeGame();
+});
+
+// Set theme on selecting a theme
+document.getElementById('themes').addEventListener('change', function() {
+    setTheme(this);
+});
---
+++ Minesweeper/assets/js/game.js
@@ -0,0 +1,356 @@
+import { NUMBER_OF_BOMBS, ROW, COL, timeElapsed } from './description.js'
+export var squares = [];
+export var run = false
+export var game_over = false;
+export var game_started = false;
+export var flags;
+export const content = document.querySelector('.content');
+export const resultBox = document.querySelector('.result-box');
+
+const msg = document.getElementById('msg');
+const board = document.getElementById('board');
+const flagBoard = document.querySelector('.flags');
+const tiles = document.getElementsByClassName('square');
+
+const victorySound = new Audio('assets/audio/victory.wav');
+const gameoverSound = new Audio('assets/audio/gameover.wav');
+var digitColors =  ['blue', 'green', 'red', 'purple', 'black', 'gray', 'maroon', 'turquoise'];
+
+// Different color for different digits on the game board
+export function setDigitColors() {
+    if (document.body.classList.contains('theme-light'))
+        digitColors =  ['blue', 'green', 'red', 'purple', 'black', 'gray', 'maroon', 'turquoise'];
+    else if (document.body.classList.contains('theme-dark'))
+        digitColors =  ['cyan', 'lime', '#fe4747', '#ff38ca', 'black', 'yellow', '#c51717', 'turquoise'];
+    else if (document.body.classList.contains('theme-nature'))
+        digitColors =  ['cyan', 'lime', 'red', '#ff38ca', 'white', 'yellow', '#c51717', 'royalblue'];
+
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            var box = squares[i][j]; 
+            if (box.number > 0 && box.status == 'clicked') {
+                box.draw.style.color = digitColors[box.number-1];
+            }
+        }
+    }
+}
+
+// Count number of revealed tiles
+export function countRevealedTiles() {
+    let countTiles = 0;
+
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            if (squares[i][j].status == 'clicked') {
+                countTiles++;
+            }
+        }
+    }
+
+    return countTiles;
+}
+
+
+// run is a boolean variable indicating the timer is running or not
+// Function is used to change value in other modules
+export function setTimer(value) {
+    run = value;
+}
+
+
+// game_over is a boolean variable to indicate if game is over
+// Function is used to change value in other modules
+export function setGameOver(value) {
+    game_over = value;
+}
+
+
+// Set the number of placable flags to default
+export function resetFlags() {
+    flags = NUMBER_OF_BOMBS;
+    flagBoard.innerHTML = flags;
+}
+
+
+// Placing bomb after first click so that first click will never trigger a bomb
+function firstClick(i, j) {
+    game_started = true;
+    setGameOver(false);
+    placeBombs(i, j);
+    placeNumbers();
+    setTimer(true);
+}
+
+
+// Draw the board according to number of rows and columns
+export function drawBoard() {
+    game_started = false;
+    board.innerHTML = '';
+    squares.length = 0;
+
+    for (let i=0; i<ROW; i++) {
+        squares.push([]);
+        for (let j=0; j<COL; j++) {
+            // An object for each tile.
+            // 1. number: number of bombs around a tile. -1 means this tile itself a bomb
+            // 2. draw: used for UI design
+            // 3. status:
+            //     'clickable' means player can click on this tile.
+            //     'clicked' means this tile can't be clicked (already clicked/game over).
+            //     'flagged' means this tile has been flagged as bomb and hence can't be clicked unless it's unflagged
+
+            var block = {
+                number: 0,
+                draw: createSquare(i*25, j*25, i, j),
+                status: 'clickable'
+            }
+            squares[i].push(block);
+        }
+    }
+}
+
+
+// Put bombs in random cells
+function placeBombs(fi, fj) {
+    for (let i=0; i<NUMBER_OF_BOMBS; i++) {
+        // Selecting a random cell
+        var randomY = Math.floor(Math.random() * ROW);
+        var randomX = Math.floor(Math.random() * COL);
+
+        // Bomb can't be where there is already a bomb or the cell which was clicked at first
+        if (squares[randomY][randomX].number == -1 || (randomY == fi && randomX == fj)) {
+            i--;
+        }
+
+        // -1 indicactes a bomb
+        else {
+            squares[randomY][randomX].number = -1;
+        }
+    }
+}
+
+
+// Count bombs adjacence to a tile and assign the number of bombs in the tile
+function placeNumbers() {
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            // For perimeter tiles
+            var surroundArea = {
+                topY: 0,
+                leftX: 0,
+                bottomY: 0,
+                rightX: 0
+            }
+
+            surroundArea.topY = Math.max(i-1, 0);
+            surroundArea.leftX = Math.max(j-1, 0);
+            surroundArea.bottomY = Math.min(i+1, ROW-1);
+            surroundArea.rightX = Math.min(j+1, COL-1);
+
+            // If a tiles contains any bomb, don't assign any number to it
+            if (squares[i][j].number !== -1) {
+                for (let x=surroundArea.topY; x<surroundArea.bottomY + 1; x++) {
+                    for (let y=surroundArea.leftX; y<surroundArea.rightX + 1; y++) {
+                        if (squares[x][y].number == -1)
+                            squares[i][j].number++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+// Create indivual square in the board
+function createSquare(top, left, posX, posY) {
+    var sqr = document.createElement('div');
+    sqr.style.top = top + 'px';
+    sqr.style.left = left + 'px';
+    sqr.classList.add('square');
+    board.appendChild(sqr);
+
+    return sqr;
+}
+
+
+// When a bomb is triggered, reveal all other bombs in the gameboard
+function revealAllBombs(eX, eY) {
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            // Bombs under flags won't be revealed when a bomb is triggered
+            if (squares[i][j].number == -1 && squares[i][j].status != 'flagged') {
+                squares[i][j].draw.innerHTML = '<i class="fas fa-bomb"></i>';
+                squares[i][j].draw.classList.add('revealed');
+                squares[i][j].draw.style.fontSize = '16px';
+            }
+        }
+    }
+}
+
+
+// Reveal a particular square on click
+function revealSquare(x, y) {
+    var box = squares[x][y];
+
+    if (box.status != 'clickable') {
+        return;
+    }
+
+    box.draw.classList.add('revealed');
+
+    // Reveal all nearby boxes if clicked on an empty cell
+    if (box.number == 0) {
+        box.status = 'clicked'; 
+
+        // For cells in the perimeter
+        var surroundArea = {
+            topY: 0,
+            leftX: 0,
+            bottomY: 0,
+            rightX: 0
+        }
+
+        surroundArea.topY = Math.max(x-1, 0);
+        surroundArea.leftX = Math.max(y-1, 0);
+        surroundArea.bottomY = Math.min(x+1, ROW-1);
+        surroundArea.rightX = Math.min(y+1, COL-1);
+
+        // Reveal neighbouring tiles of an empty tile
+        for (let x1=surroundArea.topY; x1<surroundArea.bottomY + 1; x1++) {
+            for (let y1=surroundArea.leftX; y1<surroundArea.rightX + 1; y1++) {
+                revealSquare(x1, y1);
+            }
+        }
+    }
+
+    // Reveal boxes with a number
+    else if (box.number > 0) {
+        box.draw.innerHTML = box.number;
+        box.draw.style.color = digitColors[box.number-1];
+        box.status = 'clicked';  
+    }
+
+    // Clicked on a bomb. Reveal all other bombs
+    else if (box.number == -1) {
+        setTimer(false);
+        box.draw.innerHTML = '<i class="fas fa-bomb"></i>';
+        box.draw.classList.add('bombed');
+        revealAllBombs(x, y);
+        gameover('Lost');
+    }
+}
+
+
+// Flag/unflag tiles
+function flagSquare(x, y, end=false) {
+    var box = squares[x][y];
+
+    // If tile isn't flagged, add a flag
+    if (box.status == 'clickable') {
+        flags--;
+        flagBoard.innerHTML = flags;
+        box.status = 'flagged';
+        box.draw.classList.add('flagged');
+        box.draw.innerHTML = '<i class="fas fa-flag"></i>';
+    }
+
+    // If a tile is already flagged, remove the flag
+    else if (box.status == 'flagged' && !end) {
+        flags++;
+        flagBoard.innerHTML = flags;
+        box.status = 'clickable';
+        box.draw.classList.remove('flagged');
+        box.draw.innerHTML = '';
+    }
+}
+
+// Flag all bombs when player wins
+export function flagAllSquares() {
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            if (squares[i][j].number == -1)
+                flagSquare(i, j, true);
+        }
+    }
+}
+
+
+// Unflag all squares when a game is over
+export function unflagAllSquares() {
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            squares[i][j].draw.classList.remove('flagged');
+        }
+    }
+}
+
+
+// Gameover popup message. tag contains result (win/lose)
+export function gameover(tag) {
+    setGameOver(true);
+
+    // h1 colors for different result
+    const res = {
+        'Lost': ['#bc2130', gameoverSound],
+        'Won': ['#28a745', victorySound]
+    }
+
+    // Play victory/defeat sound
+    res[tag][1].play();
+
+    // Make all the tiles unclickable after win/lose
+    for (let i=0; i<ROW; i++) {
+        for (let j=0; j<COL; j++) {
+            squares[i][j].status = 'clicked';
+        }
+    }
+
+    // Result popup shown after 3 seconds
+    setTimeout(() => {
+        document.getElementById('timeSpent').innerHTML = timeElapsed;
+        msg.innerHTML = 'You ' + tag;
+        msg.style.color = res[tag][0];
+        resultBox.style.display = 'flex';
+        content.classList.add('blurred');
+    }, 3000);
+}
+
+
+// Create a board with window loading and reset flags to default
+window.onload = () => {
+    drawBoard();
+    resetFlags();
+    setBtnProperties();
+}
+
+
+// Prevent right mouse button from working except flagging
+// window.addEventListener('contextmenu', function (e) { 
+//     e.preventDefault(); 
+// }, false);
+
+
+// Options are allowed to click after board is created
+export function setBtnProperties() {
+    setTimeout(() => {
+        for (let i=0; i<ROW; i++) {
+            for (let j=0; j<COL; j++) {
+                const c = tiles[COL*i + j];
+
+                // Reveal square on LEFT MOUSE CLICK
+                c.addEventListener('click', function() {
+                    // Indicates if it's the first click
+                    if (!game_started) {
+                        firstClick(i, j);
+                    }
+                    revealSquare(i, j);
+                });
+        
+                // Mark a tile as flagged on RIGHT MOUSE CLICK
+                c.addEventListener('contextmenu', function() {
+                    flagSquare(i, j);
+                });
+            }
+        }        
+    }, 1000);
+}
