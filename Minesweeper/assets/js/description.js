05280871fafa850b158129b7e11dac4d77d98de1
AldrinSaurovSarker <53370488+AldrinSaurovSarker@users.noreply.github.com>
2021-07-14 18:18:56 +0600

    Update description.js

--- Minesweeper/assets/js/description.js
+++ Minesweeper/assets/js/description.js
@@ -32,21 +32,31 @@ function setThemeProperties() {
     if (document.body.classList.contains('theme-light')) {
         digitColors =  ['blue', 'green', 'red', 'purple', 'black', 'gray', 'maroon', 'turquoise'];
         bombType = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M23,13V11H19.93C19.75,9.58 19.19,8.23 18.31,7.1L20.5,4.93L19.07,3.5L16.9,5.69C15.77,4.81 14.42,4.25 13,4.07V1H11V4.07C9.58,4.25 8.23,4.81 7.1,5.69L4.93,3.5L3.5,4.93L5.69,7.1C4.81,8.23 4.25,9.58 4.07,11H1V13H4.07C4.25,14.42 4.81,15.77 5.69,16.9L3.5,19.07L4.93,20.5L7.1,18.31C8.23,19.19 9.58,19.75 11,19.93V23H13V19.93C14.42,19.75 15.77,19.19 16.9,18.31L19.07,20.5L20.5,19.07L18.31,16.9C19.19,15.77 19.75,14.42 19.93,13H23M12,8A4,4 0 0,0 8,12H6A6,6 0 0,1 12,6V8Z" /></svg>';
+        bombColor = '#000000';
     }
 
     else if (document.body.classList.contains('theme-dark')) {
         digitColors =  ['cyan', 'lime', '#fe4747', '#ff38ca', 'black', 'yellow', '#c51717', 'turquoise'];
         bombType = '<i class="fas fa-bomb"></i>';
+        bombColor = '#000000';
     }
 
     else if (document.body.classList.contains('theme-nature')) {
         digitColors =  ['cyan', 'lime', 'red', '#ff38ca', 'white', 'yellow', '#c51717', 'royalblue'];
-        bombType = '<i class="las la-radiation" style="color: yellow"></i>';
+        bombType = '<i class="las la-radiation"></i>';
+        bombColor = '#ffff00';
     }
 
     else if (document.body.classList.contains('theme-fire')) {
         digitColors =  ['orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange'];
         bombType = '<i class="fas fa-fire-alt"></i>';
+        bombColor = '#ff0000';
+    }
+
+    else if (document.body.classList.contains('theme-ice')) {
+        digitColors =  ['whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke'];
+        bombType = '<i class="fas fa-snowflake"></i>';
+        bombColor = '#c6fbff';
     }
 
     for (let i=0; i<ROW; i++) {
@@ -135,7 +145,7 @@ function restartCurrentLevel() {
         for (let j = 0; j < COL; j++) {
             squares[i][j].status = 'clickable';
             squares[i][j].draw.innerHTML = '';
-            squares[i][j].draw.classList.remove('revealed', 'bombed');
+            squares[i][j].draw.classList.remove('revealed', 'bombed', 'bomb');
             squares[i][j].draw.style.color = '#000000';
         }
     }
